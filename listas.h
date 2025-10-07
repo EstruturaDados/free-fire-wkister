9a4182a23c492d7efd06b6bfb451c273dcb3c4f6
Wellington Kister do Nascimento <wkister@gmail.com>
2025-10-07 05:47:12 -0300

    Inicio das listas encadeadas

--- listas.h
+++ listas.h
@@ -1,4 +1,12 @@
 #ifndef LISTA_H
 #define LISTA_H
 
+// Estrutrura para representar um nó na lista encadeada
+typedef struct noMochila {
+    char nome[TAM_STRING]; // Nome do item
+    char tipo[TAM_STRING]; // Tipo do item (Arma, Munição, Cura, Equipamento, Outro)
+    int quntidade; // Quantidade do item
+    struct No* proximo; // Ponteiro para o próximo nó na lista
+} noMochila;
+
 #endif
